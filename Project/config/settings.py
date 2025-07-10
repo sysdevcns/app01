49e26bf28dc27e9d532c3048cb76b4eef59eee58
sysdevcns <sidney.moraes07@gmail.com>
2025-07-10 17:06:27 -0300

    Delete Project directory

--- Project/config/settings.py
+++
@@ -1,8 +0,0 @@
-class Settings:
-    DB_CONFIG = {
-        'dbname': 'railway',
-        'user': 'postgres',
-        'password': 'XDPmdhUxrkgNaHqTtqOSxAxjZfSsYkfV',
-        'host': 'autorack.proxy.rlwy.net',
-        'port': '36858'
-    }
