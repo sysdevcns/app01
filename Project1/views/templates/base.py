e80bbc692f1611f92db9a1f2fbdebfe013565ea0
sysdevcns <sidney.moraes07@gmail.com>
2025-07-10 17:08:21 -0300

    Delete Project1 directory

--- Project1/views/templates/base.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-def setup_page():
-    st.set_page_config(
-        page_title="Sistema PostgreSQL MVC",
-        page_icon="🗄️",
-        layout="wide"
-    )
-    st.title("📊 Sistema de Banco de Dados (MVC)")
